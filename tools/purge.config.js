906d5e22ccfaca9d2f7db971e9776c073c6c5337
Kevin Tresuelo <kevztresuelo@gmail.com>
2021-08-28 08:19:56 +0800

    Add files via upload

---
+++ tools/purge.config.js
@@ -0,0 +1,29 @@
+(Math.sign() != -1 ? '+' : '') + (parseFloat(tentative_cyl) + ((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.abs(jcc_power_pos1 - jcc_power_pos2) * 0.25)).toFixed(2)
+
+
+
+
+
+
+
+(Math.sign((parseFloat(tentative_sph)+((((jcc_power_pos1 > jcc_power_pos2) ? 1 : -1) * (Math.floor(Math.abs(((i*((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1)) * 0.25)/-0.50))))*0.25))) != -1 ? '+' : '') + (parseFloat(tentative_sph)+((((jcc_power_pos1 > jcc_power_pos2) ? 1 : -1) * (Math.floor(Math.abs(((i*((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1)) * 0.25)/-0.50))))*0.25)).toFixed(2)
+
+
+
+(parseFloat(tentative_sph)+((((jcc_power_pos1 > jcc_power_pos2) ? 1 : -1) * (Math.floor(Math.abs(((i*((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1)) * 0.25)/-0.50))))*0.25))
+
+
+
+
+((Math.sign((parseFloat(tentative_cyl)+((i*((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1)) * 0.25))) != -1 ? '+' : '') + (parseFloat(tentative_cyl)+((i*((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1)) * 0.25)).toFixed(2))
+
+(parseFloat(tentative_cyl)+((i*((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1)) * 0.25))
+
+
+
+
+(parseFloat(tentative_cyl) + ((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.abs(jcc_power_pos1 - jcc_power_pos2) * 0.25)).toFixed(2)
+
+((Math.sign(parseFloat(tentative_sph) + ((((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.floor(Math.abs((((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.abs(jcc_power_pos1 - jcc_power_pos2) * 0.25)))/0.50))) * -0.25)) != -1 ? '+' : '') + (parseFloat(tentative_sph) + ((((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.floor(Math.abs((((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.abs(jcc_power_pos1 - jcc_power_pos2) * 0.25)))/0.50))) * -0.25)).toFixed(2))
+
+(parseFloat(tentative_sph) + ((((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.floor(Math.abs((((jcc_power_pos1 > jcc_power_pos2) ? -1 : 1) * (Math.abs(jcc_power_pos1 - jcc_power_pos2) * 0.25)))/0.50))) * -0.25))
